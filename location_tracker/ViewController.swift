c872e4200375447581cc3d355672775d7c9b0fa2
Arkadiusz <arkadiuszdowejko@arkadiuszs-imac.local>
2017-08-25 18:22:49 +0100

    Initial Commit

---
+++ location_tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  location_tracker
+//
+//  Created by Arkadiusz Dowejko on 25/08/2017.
+//  Copyright © 2017 Arkadiusz Dowejko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
